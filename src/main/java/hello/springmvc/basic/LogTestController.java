5e73a53dd8f951efbbf36f22f607af99172c86a0
SeungHwan Oh <wxtq22@naver.com>
2024-07-29 15:35:13 +0900

    first commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name); // 디버그 할때 보는 정보
+        log.info(" info log={}", name); // 현재로그는 중요한 정보
+        log.warn(" warn log={}", name); // 위험한 정보
+        log.error("error log={}", name); // 에러 정보
+
+        //로그를 사용하지 않아도 a+b 계산 로직이 먼저 실행됨, 이런 방식으로 사용하면 X
+        log.debug("String concat log=" + name);
+        return "ok";
+    }
+}
